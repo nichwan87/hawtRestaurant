f0b34389acfeee10e3c434dcdb02daf6d15f015b
Dylan Rooney <dylantrooney1@gmail.com>
2021-05-06 19:56:22 +1000

    fixed syntaxes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Dependencies
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // port the server will be using
 const PORT = 8080;
@@ -8,9 +8,9 @@ const PORT = 8080;
 //sample text for what the user see's
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
 // showing that the server is running
 app.listen(PORT, () => {
     console.log(`listening on: ${PORT}`)
-})
+});
